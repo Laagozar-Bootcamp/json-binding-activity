f16cbaedcbf33cd6b8d568883c97b3fcb075b869
Lea Gozar <lea.adelle.gozar@accenture.com>
2025-08-08 06:24:33 +0000

    Add bindings

--- webapp/controller/JSONBinding.controller.js
+++ webapp/controller/JSONBinding.controller.js
@@ -10,14 +10,38 @@ sap.ui.define([
                 "EID": "lea.adelle.gozar",
                 "Enabled": true,
                 "Address": {
-                    "Street": "Kilometer",
+                    "Street": "Kilometer Street",
                     "City": "Fake City",
-                    "ZIP": "0000",
+                    "ZIP": "1000",
                     "Country": "Any country",
                 },
-                "Sales Amount": "99999999",
+                "SalesAmount": "888888",
                 "CurrencyCode": "PHP",
             });
+
+            var oSimpleForm1 = this.getView().byId("form1");
+            oSimpleForm1.setModel(oModel);
+
+            var oSimpleFormAdr = this.getView().byId("addForm");
+            oSimpleFormAdr.setModel(oModel);
+
+            var oList1 = this.getView().byId("addList");
+            oList1.setModel(oModel);
+
+            //sets inputs (EID, Sales Amount) with enabled property = false
+            this.getView().byId("eidInp").setProperty("enabled",false);
+            this.getView().byId("salesAmtInp").setProperty("enabled",false);
+
+            var oSimpleFormProd = this.getView().byId("prodDetForm");
+            var oProdModel = new JSONModel("./model/ProductsModel.json");
+            oSimpleFormProd.setModel(oProdModel);
+        },
+        onSelectItem: function(oEvt) {
+            var oSelectItm = oEvt.getSource();
+            var oBindContext = oSelectItm.getBindingContext("ProductsModel");
+            var sPath = oBindContext.getPath();
+            var oProdDetForm = this.byId("prodDetForm");
+            oProdDetForm.bindElement({ path: sPath, model: "ProductsModel" });
         }
     });
 });
